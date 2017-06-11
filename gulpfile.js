c633fa496e902113a59e6dc4bb9d61029b147e3b
hideo54 <hideo54@hideo54.com>
2017-06-11 23:41:46 +0900

    Add gulp plumber to avoid stopping gulp watch on error

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
+const plumber = require('gulp-plumber');
 const pug = require('gulp-pug');
 const less = require('gulp-less');
 const cleanCSS = require('gulp-clean-css');
 
 gulp.task('pug', () => {
     return gulp.src('src/**/*.pug')
+        .pipe(plumber())
         .pipe(pug({
             locals: require('./config.json')
         }))
@@ -13,14 +15,15 @@ gulp.task('pug', () => {
 
 gulp.task('less', () => {
     return gulp.src('src/style.less')
+        .pipe(plumber())
         .pipe(less())
         .pipe(cleanCSS())
         .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('watch', ['pug', 'less'], () => {
-    gulp.watch('src/**/*.pug', 'pug');
-    gulp.watch('src/style.less', 'less');
+    gulp.watch('src/**/*.pug', ['pug']);
+    gulp.watch('src/style.less', ['less']);
 });
 
 gulp.task('default', ['pug', 'less']);
