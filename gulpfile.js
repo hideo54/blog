96d7be7fd1eb666b5e9394d8aac7c71d35d44dcf
hideo54 <hideo54@hideo54.com>
2017-06-25 00:01:48 +0900

    Imprement Recent Articles menu on the top page

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,44 @@
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
-const pug = require('gulp-pug');
+const gulpPug = require('gulp-pug');
 const less = require('gulp-less');
 const cleanCSS = require('gulp-clean-css');
+const fs = require('fs');
+const pug = require('pug');
+const cheerio = require('cheerio');
+
+const getRecentPosts = num => {
+    return new Promise((resolve, reject) => {
+        const files = fs.readdirSync('src/archives/').reverse().slice(0, num);
+        let recent = [];
+        for (filename of files) {
+            const html = pug.renderFile(`src/archives/${filename}`, {
+                config: require('./config.json'),
+                basedir: 'src/'
+            });
+            const dom = cheerio.load(html);
+            const title = dom('title').text();
+            const pageNum = filename.slice(0, filename.length - 4);
+
+            recent.push({ number: pageNum, title: title });
+        }
+        resolve(recent);
+    });
+};
 
 gulp.task('pug', () => {
-    return gulp.src(['src/**/[^_]*.pug'])
-        .pipe(plumber())
-        .pipe(pug({
-            locals: {
-                config: require('./config.json')
-            },
-            basedir: 'src/'
-        }))
-        .pipe(gulp.dest('dist/'));
+    getRecentPosts(5).then((recent) => {
+        return gulp.src(['src/**/[^_]*.pug'])
+            .pipe(plumber())
+            .pipe(gulpPug({
+                locals: {
+                    config: require('./config.json'),
+                    recent: recent
+                },
+                basedir: 'src/'
+            }))
+            .pipe(gulp.dest('dist/'));
+    });
 });
 
 gulp.task('less', () => {
