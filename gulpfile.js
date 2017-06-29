2e761c29f31fffc7954afea828d23f263da6a765
hideo54 <hideo54@hideo54.com>
2017-06-30 00:38:30 +0900

    Support displaying popular articles

--- gulpfile.js
+++ gulpfile.js
@@ -6,41 +6,75 @@ const cleanCSS = require('gulp-clean-css');
 const fs = require('fs');
 const pug = require('pug');
 const cheerio = require('cheerio');
-const config = require('./config.json')
+const google = require('googleapis');
+const config = require('./config.json');
+
+const getPostData = filename => {
+    const html = pug.renderFile(`src/archives/${filename}`, {
+        config: config,
+        basedir: 'src/'
+    });
+    const dom = cheerio.load(html);
+    const title = dom('title').text();
+    const pageNum = filename.slice(0, filename.length - 4);
+    const date = dom('span.date').text();
+    return { number: pageNum, title: title, date: date };
+};
 
 const getRecentPosts = num => {
-    return new Promise((resolve, reject) => {
-        const files = fs.readdirSync('src/archives/').reverse().slice(0, num);
-        let recent = [];
-        for (filename of files) {
-            const html = pug.renderFile(`src/archives/${filename}`, {
-                config: config,
-                basedir: 'src/'
-            });
-            const dom = cheerio.load(html);
-            const title = dom('title').text();
-            const pageNum = filename.slice(0, filename.length - 4);
-            const date = dom('span.date').text();
+    const files = fs.readdirSync('src/archives/').reverse().slice(0, num);
+    let recent = [];
+    for (filename of files) {
+        const data = getPostData(filename);
+        recent.push(data);
+    }
+    return recent;
+};
 
-            recent.push({ number: pageNum, title: title, date: date });
+const getPopularPosts = num => {
+    return new Promise((resolve, reject) => {
+        if (config.numberOfPopular === 0) { resolve([]); } else {
+            const googleKey = require('./google-key.json');
+            const jwtClient = new google.auth.JWT(
+                googleKey.client_email, null, googleKey.private_key,
+                ['https://www.googleapis.com/auth/analytics.readonly'], null
+            );
+            const analytics = google.analytics('v3');
+            analytics.data.ga.get({
+                auth: jwtClient,
+                ids: config.googleAnalyticsViewId,
+                'start-date': '7daysAgo',
+                'end-date': 'today',
+                metrics: 'ga:pageviews',
+                dimensions: 'ga:pagePath',
+                filters: 'ga:pagePath=~^\/archives\/[0-9]*$',
+                sort: '-ga:pageviews'
+            }, async (err, data) => {
+                if (err) { console.log(err); } else {
+                    const popular = data.rows.slice(0, num).map(array => {
+                        return getPostData(`${array[0].slice(10)}.pug`);
+                    });
+                    resolve(popular);
+                }
+            });
         }
-        resolve(recent);
     });
 };
 
-gulp.task('pug', () => {
-    getRecentPosts(config.numberOfRecent).then((recent) => {
-        return gulp.src(['src/**/[^_]*.pug'])
-            .pipe(plumber())
-            .pipe(gulpPug({
-                locals: {
-                    config: config,
-                    recent: recent
-                },
-                basedir: 'src/'
-            }))
-            .pipe(gulp.dest('dist/'));
-    });
+gulp.task('pug', async () => {
+    const recent = await getRecentPosts(config.numberOfRecent);
+    const popular = await getPopularPosts(config.numberOfPopular);
+    return gulp.src(['src/**/[^_]*.pug'])
+        .pipe(plumber())
+        .pipe(gulpPug({
+            locals: {
+                config: config,
+                recent: recent,
+                popular: popular
+            },
+            basedir: 'src/'
+        }))
+        .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('less', () => {
