932a07e8dbc9ee0f2d7de40024dcc2fe317ce32e
hideo54 <hideo54@hideo54.com>
2021-08-07 20:14:27 +0900

    Fix (but not used now)

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,6 +3,6 @@ import dayjs, { Dayjs } from 'dayjs';
 export const stringifyDate = (date: string | Date | Dayjs) => {
     const d = dayjs(date);
     const now = dayjs();
-    const format = d.year === now.year ? 'M月D日' : 'YYYY年M月D日';
+    const format = d.year() === now.year() ? 'M月D日' : 'YYYY年M月D日';
     return d.format(format);
 };
